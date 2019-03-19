a6254dd9da2598efa21dbc2c573b7a9d919eb519
gf-huanchupk <782969359@qq.com>
2019-03-20 00:26:20 +0800

    第十四篇 : Spring Boot Security 详解

---
+++ springboot-security/src/main/java/com/gf/entity/RolePermisson.java
@@ -0,0 +1,26 @@
+package com.gf.entity;
+
+
+public class RolePermisson {
+
+    private String url;
+    private String roleName;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+
+}
---
+++ springboot-security/src/main/java/com/gf/entity/User.java
@@ -0,0 +1,84 @@
+package com.gf.entity;
+
+
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class User implements UserDetails , Serializable {
+
+    private Long id;
+    private String username;
+    private String password;
+
+    private List<Role> authorities;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public List<Role> getAuthorities() {
+        return authorities;
+    }
+
+    public void setAuthorities(List<Role> authorities) {
+        this.authorities = authorities;
+    }
+
+    /**
+     * 用户账号是否过期
+     */
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    /**
+     * 用户账号是否被锁定
+     */
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    /**
+     * 用户密码是否过期
+     */
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    /**
+     * 用户是否可用
+     */
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+}
